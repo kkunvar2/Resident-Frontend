1f68301ef0e4c75a57112c7e3be4c0d6eed2ac5d
kkunvar2 <kkunvar2@gmail.com>
2024-03-08 15:10:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GuestReport from './Components/Guest/GuestReport'
+import ComplaintTab from './Components/Complaint/ComplaintTab'
+import ComplaintForm from './Components/Complaint/ComplaintForm'
+import Feedback from './Components/Feedback/Feedback'
+import Feed from './Components/Feedback/Feed'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={<GuestReport />}/> */}
+        {/* <Route path='/' element={<ComplaintTab />}/> */}
+        <Route path='/' element={<ComplaintForm />}/>
+        {/* <Route path='/' element={<Feedback />}/> */}
+        {/* <Route path='/' element={<Feed />}/> */}
+
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Guest/GuestReport.js
@@ -0,0 +1,116 @@
+import React from 'react'
+
+
+const GuestReport = () => {
+    return (
+        <>
+            <div className="mt-12 rounded-lg overflow-x-auto">
+                <table className="sm:w-full px-12 h-[20px] min-w-max text-sm text-left rtl:  text-gray-500 dark:text-gray-400">
+                    <thead className=" sticky text-xs text-yellow-300 uppercase bg-slate-700">
+                        <tr>
+                            <th className="px-6 py-3">Guest Name</th>
+                            <th className="px-6 py-3">Post</th>
+                            <th className="px-6 py-3">In Time</th>
+                            <th className="px-6 py-3">Number</th>
+                            <th className="px-6 py-3">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-slate-200 ">
+                        <tr className="border-b hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Microsoft Surface Pro
+                            </th>
+                            <td className="px-6 py-4">White</td>
+                            <td className="px-6 py-4">Laptop PC</td>
+                            <td className="px-6 py-4">$1999</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-300">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+                            <td className="px-6 py-4  ">
+                                <p className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+        </>
+    )
+}
+
+export default GuestReport
