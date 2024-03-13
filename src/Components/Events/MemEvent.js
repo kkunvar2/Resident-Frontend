8f78d0d7fe249fe2ef0e1861959f97d49200bd10
kkunvar2 <kkunvar2@gmail.com>
2024-03-13 17:57:39 +0530

    responsive form

--- src/Components/Events/MemEvent.js
+++ src/Components/Events/MemEvent.js
@@ -61,27 +61,30 @@ const MemEvent = () => {
 return (
   <>
     <section className='h-screen  bg-gray-900 body-font'>
-      <div className='flex justify-between px-24 py-24'>
-        <form  onSubmit={handleSubmit} className='lg:w-3/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0'>
+      <div className='flex justify-between px-10 py-24'>
+        {/* form */}
+        <form  onSubmit={handleSubmit} className='lg:w-3/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col  w-[90rem] mt-10 md:mt-0'>
             <div className='flex items-center justify-between'>
               <h2 className="text-white text-lg font-medium title-font mb-5">Book Event</h2> 
             </div>
 
-            {/* Name */}
-            <div className=" mb-8">
-                <label className="leading-7 text-lg text-gray-400">Type: </label>
-                <select className='lg:w-[30%] lg:ml-4 bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"'
-                        onChange={handlechange}
-                        value={values.type}
-                        name="type"  >
-                          <option className='bg-gray-600 font-normal mb-3' >Select a Event</option>
-                              {
-                                events.map((event, i) => {
-                                  return <option key={i} value={event} className='bg-gray-600 text-yellow-500'>{event}</option>
-                                })
-                              }
-                          <option className='bg-gray-600 text-white' value='Other'><input className='text-white' type='text' placeholder='Other'/>Other</option>
-                </select>
+            {/* type */}
+            <div className="flex max-md:flex-col max-lg:flex-row mb-5  gap-3 ">
+                <div className='w-[60%]'>
+                  <label className="leading-7 text-lg text-gray-400">Type: </label>
+                  <select className='max-lg:w-[70%] lg:ml-4 bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"'
+                          onChange={handlechange}
+                          value={values.type}
+                          name="type"  >
+                            <option className='bg-gray-600 font-normal mb-3' >Select a Event</option>
+                                {
+                                  events.map((event, i) => {
+                                    return <option key={i} value={event} className='bg-gray-600 text-yellow-500'>{event}</option>
+                                  })
+                                }
+                            <option className='bg-gray-600 text-white' value='Other'><input className='text-white' type='text' placeholder='Other'/>Other</option>
+                  </select>
+                </div>
                 {values.type === "Other" && ( // Conditionally render input box if "Other" is selected
                 <input
                   type="text"
@@ -89,14 +92,14 @@ return (
                   name="otherType="
                   value={values.otherType}
                   onChange={handlechange}
-                  className='lg:w-[30%] lg:ml-4 bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 px-3 leading-8 transition-colors duration-200 ease-in-out"'
+                  className='w-[45%] max-md:w-[90%] bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 px-3 leading-8 transition-colors duration-200 ease-in-out"'
                 />
               )}
             </div>
 
             {/* Date */}
-            <div className="flex justify-between mb-4">
-              <div className='flex flex-col '>
+            <div className="flex flex-col xl:flex-row md:justify-between gap-2 mb-8">
+              <div className='flex flex-col mb-4 sm:mb-0'>
                 <label className="leading-7 text-lg font-normal mb-2 text-gray-400">Start Date:</label>
                 <input 
                     onChange={handlechange} 
@@ -104,7 +107,8 @@ return (
                     type="date"  
                     name="startDate" 
                     min={today}
-                    className="w-[20rem] bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    className="w-full 2xl:w-[20rem] xl:w-[12rem] bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
               </div>
               <div className='flex flex-col'>
                 <label className="leading-7 text-lg font-normal mb-2 text-gray-400">End date:</label>
@@ -114,12 +118,13 @@ return (
                     type="date"  
                     name="endDate"
                     min={today} 
-                    className="w-[20rem] bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    className="w-full 2xl:w-[20rem] xl:w-[12rem] bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
               </div>
             </div>
             
             {/* Total */}
-            <div className='flex justify-between items-center'>
+            <div className='flex justify-between lg:items-center flex-col lg:flex-row gap-10'>
               <div className='flex flex-col gap-2 items-start'>
                 <button  className='text-white bg-yellow-500 border-0 py-2 px-2 w-42 focus:outline-none hover:bg-yellow-600 rounded text-lg'
                         onClick={handleCheck}
@@ -139,10 +144,10 @@ return (
 
         </form>
 
-        <div className='lg:w-2/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0'>
+        {/* <div className='lg:w-2/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0'>
           <h1 className="title-font font-medium text-3xl text-white">Make your Booking for a Events and Funcions</h1>
           <p className=" text-gray-400 leading-relaxed mt-4">If secretary is approved your reservation than you have been eligible for celebrating <span className='text-yellow-500'>Have great day</span> </p>
-        </div>
+        </div> */}
 
       </div>
     </section>
