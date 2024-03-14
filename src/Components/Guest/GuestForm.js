984dc68dd98cde8f70790c3c65e1f13a3a464396
kkunvar2 <kkunvar2@gmail.com>
2024-03-14 17:48:10 +0530

    flat and wing

--- src/Components/Guest/GuestForm.js
+++ src/Components/Guest/GuestForm.js
@@ -39,7 +39,8 @@ const GuestForm = () => {
                 navigate("/guestreport")
                 return res.json();
             }else{
-                throw new Error('Failed to check in');
+                console.log(res)
+                // throw new Error('Failed to check in');
             }
         })
         
@@ -49,20 +50,19 @@ const GuestForm = () => {
         const { name, value } = e.target;
         setFormdata({ ...formdata, [name]: value });
     };
+
     const handleWing = (e) => {
-      setSelWing(e.target.value);
-      setSelFloor('');
+        setFormdata({ ...formdata, wing: e.target.value });     
     };
   
     const handleFloor = (e) => {
-      setSelFloor(e.target.value);
-      console.log(selfloor)
+        setFormdata({ ...formdata, flat: e.target.value });      
     };
 
   return (
     <>
       <div className='flex justify-center items-center '>
-            <div className=' drop-shadow-xl max-w-[40rem] w-full md:h-[28rem] py-5 bg-slate-100 rounded-xl flex justify-center items-center flex-col gap-4'>
+        <form onSubmit={handleCheckIn} className=' drop-shadow-xl max-w-[40rem] w-full md:h-[28rem] py-5 bg-slate-100 rounded-xl flex justify-center items-center flex-col gap-4'>
             
             <div className='flex justify-between items-center gap-32 mb-4'>
                 <div className='flex'> <span className='text-2xl font-bold text-sky-500'>L</span>/ <span className='text-2xl font-bold text-yellow-500'>W</span> </div>
@@ -81,7 +81,7 @@ const GuestForm = () => {
                         type='tel' 
                         placeholder='Phone Number'
                         name='mobile'
-                        value={formdata.number}
+                        value={formdata.mobile}
                         onChange={handleChange}/>
                 </div>
                 {/* purpose */}
@@ -95,37 +95,34 @@ const GuestForm = () => {
                 </div>
                 {/* Flat & wing */}
                 <div className='dropdown px-10 mt-6'>
-                    <div className='menu-field flex flex-col gap-2 md:flex-row'>
+                <div className='menu-field flex flex-col gap-2 md:flex-row'>
                     <label className="bg-blue-100 text-blue-500 text-sm font-normal  px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Wing:</label>
-                        <select onChange={handleWing}>
+                    <select onChange={handleWing}>
                         <option value="">Select a Wing</option>
-                            {
-                            wings.map(state => {
-                                return <option>{state}</option>
-                                })
-                            }
-
-                        </select>
-                    </div>
-                    <div className='menu-field flex flex-col gap-2 md:flex-row'>
-                        <label className="bg-blue-100 text-blue-500 text-sm font-normal  px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Floor:</label>
-                        {selwing && <select onChange={handleFloor}>
-                            <option value="">Select a Floor</option>
-                            {
-                                floors[selwing].map(floor => {
-                                return <option>{floor}</option>
-                                })
-                            }
-                            </select>}
-                    </div>
+                        {wings.map((wing, index) => (
+                            <option key={index} value={wing}>{wing}</option>
+                        ))}
+                    </select>
                 </div>
+                <div className='menu-field flex flex-col gap-2 md:flex-row'>
+                    <label className="bg-blue-100 text-blue-500 text-sm font-normal  px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Floor:</label>
+                    {formdata.wing && (
+                        <select onChange={handleFloor}>
+                            <option value="">Select a Floor</option>
+                            {floors[formdata.wing].map((floor, index) => (
+                                <option key={index} value={floor}>{floor}</option>
+                            ))}
+                        </select>
+                    )}
+                </div>
+            </div>
                 {/* Check in */}
                 <button className='flex items-center bg-sky-500 px-9 py-2 mt-7 rounded-full text-white font-normal'
-                        onClick={handleCheckIn}>
+                        type='submit'>
                     Check In
                     {/* <img src='' alt="Next Icon" className=" ml-3 w-6 h-6"/> */}
                     </button>
-            </div>
+            </form>
         </div>      
     </>
   )
